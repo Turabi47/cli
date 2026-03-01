494a955eb62fb7bd7f000d02e2e92212d1872f2c
curious <akturabi47@gmail.com>
2026-03-01 12:17:05 +0500

    sfl

--- bin/cli.dart
+++ bin/cli.dart
@@ -1,22 +1,22 @@
 const version = '0.0.1'; // Add this line
 void main(List<String> arguments) {
-  if (arguments.isEmpty || arguments.first == 'help') {
-    printUsage(); // Change this from 'Hello, Dart!'
+  if (arguments.isEmpty) {
+    print('Hello, Dart!');
   } else if (arguments.first == 'version') {
-    print('Dartpedia CLI vsfmsdjfersion $version');
-  } else {
-    printUsage();
+    print('Dartpedia CLI version $version');
   }
 }
-
-
-
 
 void printUsage() { // Add this new function
   print(
       "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
   );
 }
+
+
+
+
+
 
 
 
